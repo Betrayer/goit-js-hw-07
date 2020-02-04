0fc3077de6ce618168e8d5951645c01e5d5fa82e
Betrayer <StarDestroyer@ukr.net>
2020-02-04 17:23:08 +0200

    hw-07_done

--- src/task-01.js
+++ src/task-01.js
@@ -4,9 +4,7 @@ const itemsAmount = categories.querySelectorAll(".item");
 
 console.log(`В списке ${itemsAmount.length} категории`);
 
-const transform = Array.from(itemsAmount);
-
-transform.forEach(header => {
+itemsAmount.forEach(header => {
   console.log(`Категория: ${header.querySelector("h2").textContent}`);
   console.log(
     `Количество элементов: ${header.querySelector("ul").children.length}`
