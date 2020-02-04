cf3393e563c238832cf827f5348aacc9e9c765de
Betrayer <StarDestroyer@ukr.net>
2020-02-04 14:23:03 +0200

    hw-07_still_in_progress

--- src/task-04.js
+++ src/task-04.js
@@ -1,1 +1,20 @@
 "use script";
+
+let counterValue = 0;
+const number = document.querySelector("#value");
+
+function decrement() {
+  counterValue--;
+  number.textContent = counterValue;
+}
+
+function increment() {
+  counterValue++;
+  number.textContent = counterValue;
+}
+
+const buttonDecrement = document.querySelector("[data-action='decrement']");
+const buttonIncrement = document.querySelector("[data-action='increment']");
+
+buttonDecrement.addEventListener("click", decrement);
+buttonIncrement.addEventListener("click", increment);
