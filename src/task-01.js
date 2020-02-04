508a3384e23534738a041e1e5c1f89215f0c72c8
Betrayer <StarDestroyer@ukr.net>
2020-02-04 11:22:11 +0200

    hw-07_in_progress

--- src/task-01.js
+++ src/task-01.js
@@ -1,1 +1,13 @@
-'use strict';
+"use strict";
+
+const itemsAmount = categories.querySelectorAll(".item");
+
+console.log(`В списке ${itemsAmount.length} категории`);
+
+const transform = Array.from(itemsAmount);
+
+transform.forEach(header => {
+  console.log(header.querySelector("h2").textContent);
+});
+
+const smth = transform.map()
